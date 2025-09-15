e1f7dd1f8fc468dd53b2c97d7e06bd3de50c1e5b
Hyunseok Oh <developer@triplengames.com>
2025-09-15 13:21:44 +0900

    Fix style Error

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
 
 // Pages
 import HomePage from './pages/HomePage';
